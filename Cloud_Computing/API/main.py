52c2892b96c74951776c4d67af78fc58372c1826
Gudboi13 <vernsitesme@gmail.com>
2023-06-14 04:24:49 +0700

    Menambahakan 3 endpoint, tambah_penjualan, penjualan, detail_penjualan

--- Cloud_Computing/API/main.py
+++ Cloud_Computing/API/main.py
@@ -72,7 +72,7 @@ def firebase_register():
         return jsonify({'message': 'Registrasi Anda telah berhasil', 'UserID': user.uid})
 
     except Exception as e:
-        return jsonify({'message': 'Registrasi Anda gagal', 'error': str(e)}), 400
+        return jsonify({'error': 'Registrasi Anda gagal', 'error': str(e)}), 400
 
 
 # Endpoint /login untuk melakukan login user
@@ -90,10 +90,10 @@ def firebase_login():
             login_firebase = requests.post(login_url, json=json_request)
             return jsonify({'message': 'Anda berhasil login, Selamat datang !', "data": login_firebase.json()})
         else:
-            return jsonify({'message': 'Anda gagal melakukan login', 'error': 'User tidak ditemukan'}), 401
+            return jsonify({'error': 'Anda gagal melakukan login', 'error': 'User tidak ditemukan'}), 401
 
     except Exception as e:
-        return jsonify({'message': 'Anda gagal melakukan login', 'error': str(e)}), 401
+        return jsonify({'error': 'Anda gagal melakukan login', 'error': str(e)}), 401
 
 
 
@@ -145,13 +145,13 @@ def get_all_barang():
         sql = "SELECT * FROM data_barang"
         cursor.execute(sql)
 
-        results = cursor.fetchall()
+        data_barang = cursor.fetchall()
 
         cursor.close()
         cnx.close()
 
         data_barang = []
-        for row in results:
+        for row in data_barang:
             barang = {
                 'id': row[0],
                 'nama_barang': row[1],
@@ -169,7 +169,7 @@ def get_all_barang():
 
 # Endopint /barang/id_barang untuk mendapatkan salah satu barang dari Cloud SQL
 @app.route('/barang/<int:id>', methods=['GET'])
-def get_barang_by_id_endpoint(id):
+def get_barang_by_id(id):
     try:
         cnx = make_connection()
         cursor = cnx.cursor()
@@ -189,7 +189,112 @@ def get_barang_by_id_endpoint(id):
     except pymysql.Error as e:
         return jsonify({'error': 'Gagal mendapatkan barang', 'message': str(e)}), 500
     
-    
+
+
+# Endopint /tambah_penjualan untuk menambahkan penjualan dan detail penjualan
+@app.route('/tambah_penjualan', methods=['POST'])
+def tambah_penjualan():
+    try:
+        data = request.get_json()
+        tanggal = data['tanggal']
+        total_keseluruhan = data['total_keseluruhan']
+        daftar_barang = data['daftar_barang']
+
+        cnx = make_connection()
+        cursor = cnx.cursor()
+     
+        sql = "INSERT INTO penjualan (tanggal, total_keseluruhan) VALUES (%s, %s)"
+        cursor.execute(sql, (tanggal, total_keseluruhan))
+        id_penjualan = cursor.lastrowid
+        
+        for barang in daftar_barang:
+            id_barang = barang['id_barang']
+            jumlah = barang['jumlah']
+            total_barang = barang['total_barang']
+
+            insert_detail_penjualan_query = "INSERT INTO detail_penjualan (id_penjualan, id_barang, jumlah, total_barang) VALUES (%s, %s, %s, %s)"
+            cursor.execute(insert_detail_penjualan_query, (id_penjualan, id_barang, jumlah, total_barang))
+        
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+
+        return jsonify({'message': 'Penjualan berhasil ditambahkan'})
+
+    except Exception as e:
+        return jsonify({'error': 'Terjadi kesalahan, penjualan gagal ditambahkan', 'message': str(e)}), 500
+
+
+
+# Endpoint /penjualan untuk mendapatkan semua data dari tabel penjualan
+@app.route('/penjualan', methods=['GET'])
+def get_all_penjualan():
+    try:  
+        cnx = make_connection()
+        cursor = cnx.cursor()
+
+        sql = "SELECT * FROM penjualan"
+        cursor.execute(sql)
+        penjualan_data = cursor.fetchall()
+
+        data_penjualan = []
+
+        for penjualan in penjualan_data:
+            id_penjualan = penjualan[0]
+            tanggal = penjualan[1]
+            total_keseluruhan = penjualan[2]
+
+            data = {
+                'id_penjualan': id_penjualan,
+                'tanggal': tanggal,
+                'total_keseluruhan': total_keseluruhan
+            }   
+            data_penjualan.append(data)
+
+        cursor.close()
+        cnx.close()
+
+        return jsonify(data_penjualan)
+
+    except Exception as e:
+        return jsonify({'error': 'Gagal mendapatkan data penjualan', 'message': str(e)}), 500
+
+
+# Endpoint untuk mendapatkan data dari tabel detail_penjualan berdasarkan ID penjualan
+@app.route('/detail_penjualan/<int:id_penjualan>', methods=['GET'])
+def get_detail_penjualan_by_id(id_penjualan):
+    try:
+        cnx = make_connection()
+        cursor = cnx.cursor()
+
+        sql = "SELECT * FROM detail_penjualan WHERE id_penjualan = %s"
+        cursor.execute(sql, (id_penjualan,))
+        detail_penjualan_data = cursor.fetchall()
+
+        data_detail_penjualan = []
+
+        for detail_penjualan in detail_penjualan_data:
+            id_detail = detail_penjualan[0]
+            id_barang = detail_penjualan[2]
+            jumlah = detail_penjualan[3]
+            total_barang = detail_penjualan[4]
+
+            data = {
+                'id_detail': id_detail,
+                'id_barang': id_barang,
+                'jumlah': jumlah,
+                'total_barang': total_barang
+            }
+            data_detail_penjualan.append(data)
+
+        cursor.close()
+        cnx.close()
+
+        return jsonify(data_detail_penjualan)
+
+    except Exception as e:
+        return jsonify({'error': 'Gagal mendapatkan detail penjualan', 'message': str(e)}), 500
+
 
 
 # Bagian Machine Learning untuk prediksi
